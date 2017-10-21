058d59c0eff24d203c06cb850a4f589ce751975d
Chris Milliano <MillianoC@gmail.com>
2017-10-21 00:37:27 -0500

    FEAT: Init an NE-ng app

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+
+
+// TODO: This is where all server/api routing will happen
+
+
+
+// Static serve all files in ng generated dist folder
+app.use(express.static(__dirname + '/itsmy-fyi-site/dist'));
+
+
+// NOTE: This needs to be the last route since it is a wildcard
+// Pass thru all other routes to the frontend ng router
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/itsmy-fyi-site/dist/index.html');
+});
+
+
+// Start server
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`App listening on port: ${PORT}`);
+});
